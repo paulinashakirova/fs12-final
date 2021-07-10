82326053d37f0651ce4def0cc8c6d4f4505ffdcd
Paulina <paulinashakirova@gmail.com>
2021-07-10 14:01:59 +0200

    nothing

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,38 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var models = require("../models");
-var bcrypt = require("bcrypt");
-var jwt = require("jsonwebtoken");
-const { Router } = require("express");
+var models = require('../models');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+const { Router } = require('express');
 const saltRounds = 10;
-var fs = require("fs/promises");
-var path = require("path");
-const { v4: uuidv4 } = require("uuid");
-var mime = require("mime-types");
-const { profile } = require("console");
-const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
+var fs = require('fs/promises');
+var path = require('path');
+const { v4: uuidv4 } = require('uuid');
+var mime = require('mime-types');
+const { profile } = require('console');
+const userShouldBeLoggedIn = require('../guards/userShouldBeLoggedIn');
 
-require("dotenv").config();
+require('dotenv').config();
 const supersecret = process.env.SUPER_SECRET;
 
 /* GET users listing. */
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
 	try {
 		const users = await models.User.findAll({
 			attributes: [
-				"id",
-				"name",
-				"email",
-				"password",
-				"address",
-				"phone",
-				"trusted_contact",
-				"trusted_name",
-				"profile_photo",
-				"latitude",
-				"longitude",
-				"location_token",
-			],
+				'id',
+				'name',
+				'email',
+				'password',
+				'address',
+				'phone',
+				'trusted_contact',
+				'trusted_name',
+				'profile_photo',
+				'latitude',
+				'longitude',
+				'location_token'
+			]
 			// include: { model: models.Album, attributes: ['name'] }
 		});
 		res.send(users);
@@ -42,20 +42,30 @@ router.get("/", async (req, res) => {
 });
 
 // GET one user
-router.get("/:id", async (req, res) => {
-	const { id } = req.params;
+router.get('/', userShouldBeLoggedIn, async (req, res) => {
 	try {
 		const user = await models.User.findOne({
-			where: { id },
+			where: { id: payload.user_id }
 		});
 		res.send(user);
 	} catch (err) {
 		res.status(500).send(err);
 	}
 });
+// router.get("/:id", async (req, res) => {
+// 	const { id } = req.params;
+// 	try {
+// 		const user = await models.User.findOne({
+// 			where: { id },
+// 		});
+// 		res.send(user);
+// 	} catch (err) {
+// 		res.status(500).send(err);
+// 	}
+// });
 
 // REGISTRATION OF USER
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
 	const {
 		name,
 		email,
@@ -66,18 +76,18 @@ router.post("/register", async (req, res) => {
 		trusted_name,
 		latitude,
 		longitude,
-		location_token,
+		location_token
 	} = req.body;
 	const { profile_photo } = req.files;
-	console.log("the photo is", profile_photo);
+	console.log('the photo is', profile_photo);
 
 	const extension = mime.extension(profile_photo.mimetype);
 
-	const filename = uuidv4() + "." + extension;
+	const filename = uuidv4() + '.' + extension;
 
 	const tmp_path = profile_photo.tempFilePath;
 
-	const target_path = path.join(__dirname, "../public/img/") + filename;
+	const target_path = path.join(__dirname, '../public/img/') + filename;
 
 	try {
 		const hash = await bcrypt.hash(password, saltRounds);
@@ -85,7 +95,7 @@ router.post("/register", async (req, res) => {
 
 		// console.log(image);
 
-		console.log("this is the body", {
+		console.log('this is the body', {
 			name,
 			email,
 			password: hash,
@@ -96,7 +106,7 @@ router.post("/register", async (req, res) => {
 			profile_photo: filename,
 			latitude,
 			longitude,
-			location_token,
+			location_token
 		});
 
 		const user = await models.User.create({
@@ -110,22 +120,22 @@ router.post("/register", async (req, res) => {
 			profile_photo: filename,
 			latitude,
 			longitude,
-			location_token,
+			location_token
 		});
 
 		res.send(user);
 	} catch (err) {
-		res.status(500).send({ msg: "Please, fill in all required fields." });
+		res.status(500).send({ msg: 'Please, fill in all required fields.' });
 	}
 });
 
 //LOGIN
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
 	const { email, password } = req.body;
 
 	try {
 		const user = await models.User.findOne({
-			where: { email },
+			where: { email }
 		});
 
 		const userId = user.id;
@@ -135,12 +145,12 @@ router.post("/login", async (req, res) => {
 
 			const correctPassword = await bcrypt.compare(password, user.password);
 
-			if (!correctPassword) throw new Error("Incorrect Password");
+			if (!correctPassword) throw new Error('Incorrect Password');
 
 			const token = jwt.sign({ user_id }, supersecret);
-			res.send({ message: "Login succesful, here is your token", token });
+			res.send({ message: 'Login succesful, here is your token', token });
 		} else {
-			throw new Error("User does not exist");
+			throw new Error('User does not exist');
 		}
 	} catch (error) {
 		res.status(400).send({ message: error.message });
@@ -148,27 +158,18 @@ router.post("/login", async (req, res) => {
 });
 
 //UPDATE user's profile
-router.put("/profile", userShouldBeLoggedIn, async function (req, res, next) {
-	const {
-		name,
-		email,
-		address,
-		phone,
-		trusted_contact,
-		trusted_name,
-		latitude,
-		longitude,
-	} = req.body;
+router.put('/profile', userShouldBeLoggedIn, async function (req, res, next) {
+	const { name, email, address, phone, trusted_contact, trusted_name, latitude, longitude } = req.body;
 	const { profile_photo } = req.files;
 	const extension = mime.extension(profile_photo.mimetype);
 
-	const filename = uuidv4() + "." + extension;
+	const filename = uuidv4() + '.' + extension;
 
 	const tmp_path = profile_photo.tempFilePath;
 
-	const target_path = path.join(__dirname, "../public/img/") + filename;
+	const target_path = path.join(__dirname, '../public/img/') + filename;
 
-	console.log("i am updating this on body", req.body);
+	console.log('i am updating this on body', req.body);
 	const user = req.user;
 
 	try {
@@ -183,24 +184,24 @@ router.put("/profile", userShouldBeLoggedIn, async function (req, res, next) {
 			profile_photo: filename,
 			latitude,
 			longitude,
-			where: { id: user.id },
+			where: { id: user.id }
 		});
 
 		// console.log("this is the user:", user)
-		console.log("this is data:", data);
-		res.send({ message: "User details was updated correctly", data: data });
+		console.log('this is data:', data);
+		res.send({ message: 'User details was updated correctly', data: data });
 	} catch (error) {
 		res.status(500).send(error.message);
 	}
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
 	const { id } = req.params;
 	try {
 		await models.User.destroy({
-			where: { id },
+			where: { id }
 		});
-		res.send({ msg: "User deleted" });
+		res.send({ msg: 'User deleted' });
 	} catch (err) {
 		res.status(404).send(err);
 	}
