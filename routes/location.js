b0a90a2456a3f239bf3d2def9eacaa9494d27aa2
karenina-pecas <arq.karengallegos89@gmail.com>
2021-07-10 17:46:37 +0200

    get user and post livelocation endpoints working

--- routes/location.js
+++ routes/location.js
@@ -31,11 +31,22 @@ router.get(
 
 //here, we generate the location_token
 //send email
-//guar usershouldbe...
+
 router.post('/liveLocation', userShouldBeLoggedIn, async (req, res) => {
   const { latitude, longitude } = req.body;
+  const location_token = uuidv4();
+  const user = req.user;
+
   try {
-    await models.User.update({});
+    const response = await user.update({
+      latitude,
+      longitude,
+      location_token,
+      where: { id: user.id }
+    });
+
+    console.log('this is the response from endopoint', response);
+    res.send({ message: 'location_token successfully stored' });
   } catch (error) {
     res.status(500).send(error.message);
   }
