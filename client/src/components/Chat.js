eae2f2eed725ac0b9f95dfac21edc1a65663f43d
Yusmin Fermin <yusmifermin@gmail.com>
2021-07-14 12:05:17 +0200

    Pusher not working

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,96 +1,125 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import Pusher from "pusher-js";
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import Pusher from 'pusher-js'
+
+Pusher.logToConsole = true
+
+const pusherKey = process.env.REACT_APP_PUSHER_KEY
 
 export default function Chat() {
-	const [messages, setMessages] = useState([]);
-	const [input, setInput] = useState("");
+  const [sender, setSender] = useState(null)
+  const [messages, setMessages] = useState([])
+  const [input, setInput] = useState('')
 
-	let { sender, receiver } = useParams();
+  let { receiver } = useParams()
+  let headers
+  useEffect(() => {
+    headers = {
+      'x-access-token': localStorage.getItem('token')
+    }
+    getSender()
+  }, [])
+  useEffect(() => {
+    setMessages([])
+    getMessages()
 
-	useEffect(() => {
-		setMessages([]);
-		getMessages();
+    Pusher.logToConsole = true
 
-		Pusher.logToConsole = true;
+    var pusher = new Pusher(pusherKey, {
+      cluster: 'ap1',
+      forceTLS: true,
+      authEndpoint: '/api/chat/pusher/auth',
+      auth: {
+        headers
+      }
+    })
 
-		var pusher = new Pusher("28f10c936cccdf0afe24", {
-			cluster: "ap1",
-			forceTLS: true,
-		});
+    const ids = [sender, receiver].sort()
 
-		const ids = [sender, receiver].sort();
-		const channelName = `chat-${ids[0]}-${ids[1]}`;
+    const channelName = `private-chat-${ids[0]}-${ids[1]}`
 
-		var channel = pusher.subscribe(channelName);
-		channel.bind("message", function (data) {
-			console.log(data);
-			setMessages((messages) => [...messages, data]);
-		});
+    var channel = pusher.subscribe(channelName)
+    channel.bind('message', function (data) {
+      console.log(data)
+      setMessages((messages) => [...messages, data])
+    })
 
-		return () => {
-			pusher.unsubscribe(channelName);
-		};
-	}, [receiver, sender]);
+    return () => {
+      pusher.unsubscribe(channelName)
+    }
+  }, [receiver, sender])
 
-	const sendMessage = async () => {
-		await axios.post(`/chat/${sender}/${receiver}`, {
-			data: { message: input },
-		});
+  const getSender = async () => {
+    try {
+      const response = await axios(`/api/users/id`, {
+        headers: {
+          'x-access-token': localStorage.getItem('token')
+        }
+      })
+      setSender(response.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-		setInput("");
-	};
+  const sendMessage = async () => {
+    await axios.post(
+      `/api/chat/${receiver}`,
+      {
+        data: { message: input }
+      },
+      {
+        headers
+      }
+    )
 
-	const getMessages = async () => {
-		let { data } = await axios(`/chat/${sender}/${receiver}`);
+    setInput('')
+  }
 
-		setMessages((messages) => [...messages, ...data]);
-	};
+  const getMessages = async () => {
+    let { data } = await axios.get(`/api/chat/${receiver}`, {
+      headers
+    })
 
-	return (
-		<div className="d-flex flex-column h-100">
-			<div className="flex-grow-1 p-3">
-				{messages.map((message, index) => (
-					<div
-						key={index}
-						className={
-							message.sender_id === sender
-								? "text-right my-2"
-								: "text-left my-2"
-						}
-					>
-						<div className="">
-							<span
-								className={`px-2 py-1 rounded text-white ${
-									message.sender_id === sender ? "bg-primary" : "bg-secondary"
-								}`}
-							>
-								{message.text}
-							</span>
-						</div>
-					</div>
-				))}
-			</div>
+    setMessages((messages) => [...messages, ...data])
+  }
 
-			<div className="bg-light p-4 border-top">
-				<div className="input-group">
-					<input
-						type="text"
-						className="form-control"
-						value={input}
-						onChange={(e) => setInput(e.target.value)}
-						onKeyPress={(e) => {
-							if (e.key === "Enter") sendMessage();
-						}}
-					/>
-					<div className="input-group-append">
-						<button onClick={sendMessage} className="btn btn-outline-primary">
-							Send
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='d-flex flex-column h-100'>
+      <div className='flex-grow-1 p-3'>
+        {messages.map((message, index) => (
+          <div key={index} className={message.sender_id === sender ? 'text-right my-2' : 'text-left my-2'}>
+            <div className=''>
+              <span
+                className={`px-2 py-1 rounded text-white ${
+                  message.sender_id === sender ? 'bg-primary' : 'bg-secondary'
+                }`}>
+                {message.text}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className='bg-light p-4 border-top'>
+        <div className='input-group'>
+          <input
+            type='text'
+            className='form-control'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') sendMessage()
+            }}
+          />
+          <div className='input-group-append'>
+            <button onClick={sendMessage} className='btn btn-outline-primary'>
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
